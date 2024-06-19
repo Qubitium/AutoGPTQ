c93dce80159c82ecff3371172163269a86c17f1f
CSY-ModelCloud <csy@modelcloud.ai>
2024-06-19 11:40:14 +0800

    push format

--- tests/test_q4_exallama.py
+++ tests/test_q4_exallama.py
@@ -9,7 +9,7 @@ try:
 except ImportError as e:
     print(f"[WARNING] Could not load exllama_kernels: {e}")
 
-from auto_gptq_next import AutoGPTQNext, exllama_set_max_input_length  # noqa: E402
+from auto_gptq_next import AutoGPTQNext  # noqa: E402
 from auto_gptq_next.models._utils import autogptq_next_post_init  # noqa: E402
 from test_q4_cuda import get_diff
 from transformers import AutoTokenizer  # noqa: E402
