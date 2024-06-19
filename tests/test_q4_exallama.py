87c272b38a0e4ca6e7318ef4b4eb7cd535670c65
ZX <zx@modelcloud.ai>
2024-06-19 06:08:45 +0000

    Move the code from models/_util.py to utils/model.py

--- tests/test_q4_exallama.py
+++ tests/test_q4_exallama.py
@@ -11,7 +11,7 @@ except ImportError as e:
 
 from auto_gptq_next import AutoGPTQNext, exllama_set_max_input_length  # noqa: E402
 from auto_gptq_next.models._const import EXLLAMA_DEFAULT_MAX_INPUT_LENGTH  # noqa: E402
-from auto_gptq_next.models._utils import autogptq_next_post_init  # noqa: E402
+from auto_gptq_next.utils.model import autogptq_next_post_init  # noqa: E402
 from test_q4_cuda import get_diff
 from transformers import AutoTokenizer  # noqa: E402
 
